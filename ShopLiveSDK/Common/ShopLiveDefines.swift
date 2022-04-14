13b75c168cb068ae272ee194215f6a024aaf11be
Vincent <vincent@shoplive.cloud>
2022-04-14 14:48:36 +0900

    pip영역설정 버그 수정

--- ShopLiveSDK/Common/ShopLiveDefines.swift
+++ ShopLiveSDK/Common/ShopLiveDefines.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreMedia
 
 @objc internal final class ShopLiveDefines: NSObject {
-    static let sdkVersion: String = "1.2.3"
+    static let sdkVersion: String = "1.2.4"
     static var phase: ShopLive.Phase = .REAL
     static var url: String {
         switch phase {
