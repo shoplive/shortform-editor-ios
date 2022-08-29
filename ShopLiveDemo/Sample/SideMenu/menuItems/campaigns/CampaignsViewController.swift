17955b1990ce407e71f6c6a54314bc8ae083df86
Vincent <89621380+Vincent1Kim@users.noreply.github.com>
2022-08-29 22:04:47 +0900

    스튜디오에서 방송을 가져온 후 선택된 상태로 메인화면으로 이동 (#115)

--- ShopLiveDemo/Sample/SideMenu/menuItems/campaigns/CampaignsViewController.swift
+++ ShopLiveDemo/Sample/SideMenu/menuItems/campaigns/CampaignsViewController.swift
@@ -129,6 +129,8 @@ extension CampaignsViewController: KeySetObserver {
 
     func keysetUpdated() {
         updateTableView()
+        guard items.count > 0 else { return }
+        self.navigationController?.popViewController(animated: true)
     }
     
     func currentKeyUpdated() {
