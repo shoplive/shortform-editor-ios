bb21ed72fe1d71a0e3f32d13c3c3eaeadac7e73c
Vincent <89621380+Vincent1Kim@users.noreply.github.com>
2022-09-23 15:03:39 +0900

    빌드스크립트 업데이트 - catalyst(mac) build command 제거 (#138)
    * catalyst(mac) build command 제거
    - 지원하지않는 api 로 인한 에러발생
    * 스크립트 코드 정리

--- build_local.sh
+++ build_local.sh
@@ -10,16 +10,13 @@ mkdir build
 
 xcodebuild archive -scheme ShopLiveSDK -archivePath "${WORKING_DIR}/build/ios.xcarchive" -sdk iphoneos SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 xcodebuild archive -scheme ShopLiveSDK -archivePath "${WORKING_DIR}/build/ios_sim.xcarchive" -sdk iphonesimulator SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
-xcodebuild archive -scheme ShopLiveSDK -archivePath "${WORKING_DIR}/build/catalyst.xcarchive" -destination='generic/platform=macOS,variant=Mac Catalyst' SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
 
 # XCFramework 생성
 xcodebuild -create-xcframework \
 -framework "${WORKING_DIR}/build/ios.xcarchive/Products/Library/Frameworks/ShopLiveSDK.framework" \
 -framework "${WORKING_DIR}/build/ios_sim.xcarchive/Products/Library/Frameworks/ShopLiveSDK.framework" \
--framework "${WORKING_DIR}/build/catalyst.xcarchive/Products/Library/Frameworks/ShopLiveSDK.framework" \
 -output "${WORKING_DIR}/build/ShopLiveSDK.xcframework"
 
-
-    rm -rf "${DIST_DIR}${DIST_SDK_DIR}"
-    mkdir "${DIST_DIR}${DIST_SDK_DIR}"
-    cp -rf "${WORKING_DIR}/build/${SDK_FILENAME}" "${DIST_DIR}${DIST_SDK_DIR}${SDK_FILENAME}"
+rm -rf "${DIST_DIR}${DIST_SDK_DIR}"
+mkdir "${DIST_DIR}${DIST_SDK_DIR}"
+cp -rf "${WORKING_DIR}/build/${SDK_FILENAME}" "${DIST_DIR}${DIST_SDK_DIR}${SDK_FILENAME}"
