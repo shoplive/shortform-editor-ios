b59d7e8165667bc43ff679e5a685a535738cf975
Vincent <89621380+Vincent1Kim@users.noreply.github.com>
2022-10-26 19:12:54 +0900

    preview 상태에서 볼륨 물리버튼을 조작하면 음소거가 해제됩니다. (#159)
    * volume up 버튼이 눌리면 음소거 설정 해제하도록 적용
    
    * 볼륨 down 버튼이 눌릴때에도 음소거 해제 하도록 반영
    
    * 미리보기 일때는 음소거 해제가 되지 않도록 수정

--- ShopLiveSDK/Component/ShopLiveController.swift
+++ ShopLiveSDK/Component/ShopLiveController.swift
@@ -313,6 +313,7 @@ final class ShopLiveController: NSObject {
     
     func setSoundMute(isMuted: Bool) {
         guard let playerMuted = ShopLiveController.player?.isMuted, playerMuted != isMuted else { return }
+        if ShopLiveController.shared.isPreview && !isMuted { return } // 미리보기 일때 음소거 해제는 return
         ShopLiveController.player?.isMuted = isMuted
         ShopLiveController.webInstance?.sendEventToWeb(event: .setVideoMute(isMuted: isMuted), isMuted)
     }
