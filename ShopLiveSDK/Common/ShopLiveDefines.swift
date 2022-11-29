2edb1dda19054db7ade8ca7cd346d5b36ade1138
Vincent <vincent@shoplive.cloud>
2022-11-29 14:57:11 -0800

    버전 코드 1.3.1

--- ShopLiveSDK/Common/ShopLiveDefines.swift
+++ ShopLiveSDK/Common/ShopLiveDefines.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreMedia
 
 @objc internal final class ShopLiveDefines: NSObject {
-    static let sdkVersion: String = "1.3.1.1"
+    static let sdkVersion: String = "1.3.1"
     
     /*
     static var phase: ShopLive.Phase = .REAL {
