45de3f02ed293af426524f3a8573ed7900f07583
Vincent <89621380+Vincent1Kim@users.noreply.github.com>
2023-05-15 12:02:51 +0900

    버전 1.4.0 (#208)
    * 버전 1.4.0
    * 데모 앱 실행되는 시점에 enablePictureInPictureMode 옵션 화면 기본값을 true 설정하도록 적용

--- ShopLiveDemo/AppDelegate.swift
+++ ShopLiveDemo/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var orientationLock = UIInterfaceOrientationMask.all
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UserDefaults.standard.register(defaults: [SDKOptionType.enablePictureInPictureMode.optionKey: true])
         return true
     }
 
