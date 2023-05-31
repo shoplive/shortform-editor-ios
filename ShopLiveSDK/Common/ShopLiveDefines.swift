bdba6168bd99759d480340a464b6904e77d9d2a9
Vincent <vincent@shoplive.cloud>
2023-05-31 13:51:24 +0900

    demo qa 1.4.1(1)

--- ShopLiveSDK/Common/ShopLiveDefines.swift
+++ ShopLiveSDK/Common/ShopLiveDefines.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreMedia
 
 @objc internal final class ShopLiveDefines: NSObject {
-    static let sdkVersion: String = "1.4.0"
+    static let sdkVersion: String = "1.4.1"
     
     /*
     static var phase: ShopLive.Phase = .REAL {
