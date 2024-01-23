62523ca850c237060b793daee4638dc006dfc1c3
hassan0424 <131642763+hassan0424@users.noreply.github.com>
2024-01-23 18:43:49 +0900

    bump version to 1.5.4 (#367)

--- matrix-sdk-ios/ShopLiveSDK/Common/ShopLiveDefines.swift
+++ matrix-sdk-ios/ShopLiveSDK/Common/ShopLiveDefines.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreMedia
 
 @objc internal final class ShopLiveDefines: NSObject {
-    static let sdkVersion: String = "1.5.3"
+    static let sdkVersion: String = "1.5.4"
     
     /*
     static var phase: ShopLive.Phase = .REAL {
