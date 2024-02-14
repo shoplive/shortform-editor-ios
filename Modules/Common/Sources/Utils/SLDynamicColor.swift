ba22562886ec1be3613dccb8b90d4399ff0e89ed
hassan0424 <hassan@shoplive.cloud>
2024-02-14 16:22:40 +0900

    common 다크모드 용 Color propertyWrapper 추가

---
+++ Modules/Common/Sources/Utils/SLDynamicColor.swift
@@ -0,0 +1,35 @@
+//
+//  SLDynamicColor.swift
+//  ShopliveSDKCommon
+//
+//  Created by sangmin han on 2/14/24.
+//  Copyright © 2024 com.app. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+
+@propertyWrapper
+public struct SLDynamicColor {
+    let light : UIColor
+    let dark : UIColor
+
+    public var wrappedValue : UIColor {
+        if #available(iOS 13.0, *) {
+            return UIColor.init { traitCollection in
+                switch traitCollection.userInterfaceStyle {
+                case .dark:
+                    return dark
+                case .light:
+                    return light
+                default:
+                    return light
+                }
+            }
+        } else {
+            return light
+        }
+    }
+}
