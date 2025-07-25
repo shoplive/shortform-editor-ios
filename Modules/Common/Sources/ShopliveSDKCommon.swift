81dfe73f21f5d392f023e6f7d811fa6c64879252
Kio <kio@shoplive.cloud>
2025-07-25 13:07:39 +0900

    version 1.7.4

--- Modules/Common/Sources/ShopliveSDKCommon.swift
+++ Modules/Common/Sources/ShopliveSDKCommon.swift
@@ -218,19 +218,19 @@ extension ShopLiveCommon {
 }
 public extension ShopLiveCommon {
     @objc static var sdkVersion: String {
-        return "1.7.3"
+        return "1.7.4"
     }
     
     @objc static var playerSdkVersion : String {
-        return "1.7.3"
+        return "1.7.4"
     }
     
     @objc static var shortformSdkVersion : String {
-        return "1.7.3"
+        return "1.7.4"
     }
     
     @objc static var videoEditorSdkversion : String {
-        return "1.7.3"
+        return "1.7.4"
     }
 }
 
